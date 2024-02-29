aa95d0c8de5f12a5bfffc3a932e296a3b0e1dde6
Aman <amanjangde02@gmail.com>
2024-02-29 23:03:10 +0530

    Used HttpSession to pass data to JSP

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,16 +4,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
 
 	@RequestMapping("home")
-//	@ResponseBody -- If we want to return the string/data using return statement
-	public String home()
+	//	@ResponseBody -- If we want to return the string/data using return statement
+	// Use application.properties to set the prefix and suffix of the files which are stored in a different path other than webapp.
+	public String home(HttpServletRequest req)
 	{
-		System.out.println("Home coming");
-		return "home"; // use home.jsp without @ResponseBody to return the JSP as a servelet
+		HttpSession session = req.getSession();
+		String name = req.getParameter("name"); // name is URL parameter
+		System.out.println("Home coming - " + name);
 		
-		// Use application.properties to set the prefix and suffix of the files which are stored in a different path other than webapp.
+		session.setAttribute("name", name);
+		
+// 		Request dispatcher - Servlet and JSP shares the same request
+		
+//		req.setAttribute(name, name); one way to pass params to JSP
+		return "home"; // use home.jsp without @ResponseBody to return the JSP as a servlet
+		
+		
+		
+		
+		
 	}
 }
--- src/main/webapp/pages/home.jsp
+++ src/main/webapp/pages/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-I love java
+I love java - ${name}
 </body>
 </html>
