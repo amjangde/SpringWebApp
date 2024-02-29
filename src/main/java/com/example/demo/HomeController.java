52aa499329a5e8bb5e97a463be15a0f5c9c60777
Aman <amanjangde02@gmail.com>
2024-02-29 22:52:23 +0530

    Added comments in homecontroller

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
 	@RequestMapping("home")
+//	@ResponseBody -- If we want to return the string/data using return statement
 	public String home()
 	{
 		System.out.println("Home coming");
-		return "home";
+		return "home"; // use home.jsp without @ResponseBody to return the JSP as a servelet
+		
+		// Use application.properties to set the prefix and suffix of the files which are stored in a different path other than webapp.
 	}
 }
